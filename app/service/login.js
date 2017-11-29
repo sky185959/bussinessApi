4ab9060be814b14f7eff62244ab2c4e981f0851b
sky300 <qiyubao88@126.com>
2017-11-29 13:51:08 +0800

    修改登录接口

--- app/service/login.js
+++ app/service/login.js
@@ -6,7 +6,16 @@ class LoginService extends Service {
   // 根据用户名和密码登录
   async login(username, password) {
     const result = await this.app.mysql.get('shopdb').get('tb_users', { username, password });
-    return { error_code: result ? 0 : 1, data: result, token:'12394795hf7g' };
+    if (username === 'admin') {
+      return {
+        role: [ 'admin' ],
+        name: 'admin',
+        data: result,
+        introduction: '我是超级管理员',
+        token: 'admin',
+        avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+      };
+    }
   }
 }
 
