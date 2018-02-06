4c68e5a79faccc3adcc4096887db009455ba2fd3
broce <huishuai_good@163.com>
2018-02-06 20:12:39 +0800

    a

--- app/service/buycar.js
+++ app/service/buycar.js
@@ -8,17 +8,23 @@ class BuycarService extends Service {
 	//获取购物车列表
 	async fetch(user_id) {
 		
-	    const sql = "select c.id,c.user_id,i.id,i.shopping_id,i.count,s.goodsname,s.price,s.pic0 from tb_car c left join tb_carinfo i on c.id=i.car_id left join tb_goods s on i.shopping_id=s.id where c.user_id="+user_id+"  order by c.id desc";
+	    const sql = "select c.id,c.user_id,i.id sid,i.shopping_id,i.count,s.goodsname,s.price,s.pic0 from tb_car c left join tb_carinfo i on c.id=i.car_id left join tb_goods s on i.shopping_id=s.id where c.user_id="+user_id+"  order by c.id desc";
 	    const result = await this.app.mysql.query(sql);
 	    return { data: result };
   	}
 
   	//加入购物车
   	async add(data){
-  		const carData = {
-  			user_id:data.user_id,
+
+  		const flag = await this.isHavaCar(data.user_id);
+  		if(flag){
+  			const carRs = flag;
+  		}else{
+	  		const carData = {
+	  			user_id:data.user_id,
+	  		}
+	  		const carRs = await this.app.mysql.insert("tb_car",carData);
   		}
-  		const carRs = await this.app.mysql.insert("tb_car",carData);
   		const carInfoData = {
   			car_id:carRs.insertId,
   			shopping_id:data.shopping_id,
@@ -33,7 +39,17 @@ class BuycarService extends Service {
 	      error_code: result.affectedRows > 0 ? 0 : 1,
 	      msg: result.affectedRows > 0 ? '添加成功' : '添加失败',
 	    };
+  	}
 
+  	async update(data){
+
+  	}
+
+  	async isHavaCar(userid){
+  		const rs = await this.app.mysql.get("tb_car",{
+  			where:{user_id:userid}
+  		});
+  		return rs;
   	}
 
 
